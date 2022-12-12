e7bf54044897650de5103b5a8ebee5ba252f1586
Yaroslav <Valso96@mail.ru>
2022-12-12 23:21:41 +0600

    Двумерный массив, заполненный случайными вещественными числами.

---
+++ task47/Program.cs
@@ -0,0 +1,20 @@
+﻿void InputMatrix(float[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Random r = new Random();
+            matrix[i, j] = r.Next(-10, 10) + r.NextSingle();
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+float[,] matrix = new float[int.Parse(numbers[0]), int.Parse(numbers[1])];
+InputMatrix(matrix);
